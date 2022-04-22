112102e94743cfa138dcb05208f93d2bb7760178
Christopher Peplin <chris.peplin@rhubarbtech.com>
2022-04-22 15:30:48 -0400

    Import changes from original AtoB hard fork

--- lib/trestle/omniauth/engine.rb
+++ lib/trestle/omniauth/engine.rb
@@ -1,26 +1,24 @@
-require "omniauth"
+require 'omniauth'
 
 module Trestle
   module Omniauth
     class Engine < ::Rails::Engine
       isolate_namespace Trestle::Omniauth
 
-      config.assets.precompile << "trestle/omniauth.css" << "trestle/omniauth/userbox.css"
+      config.assets.precompile << 'trestle/omniauth.css' << 'trestle/omniauth/userbox.css'
 
       config.before_initialize do
-        Trestle::Engine.paths["app/helpers"].concat(paths["app/helpers"].existent)
+        Trestle::Engine.paths['app/helpers'].concat(paths['app/helpers'].existent)
       end
 
-      initializer "trestle.omniauth.extend_controller" do
-        Trestle::ApplicationController.send(:include, Trestle::Omniauth::ControllerMethods)
+      config.to_prepare do
+        Trestle::ApplicationController.include Trestle::Omniauth::ControllerMethods
       end
 
-      initializer "trestle.omniauth.configure" do
+      initializer 'trestle.omniauth.configure' do
         Trestle::Engine.middleware.use ::OmniAuth::Builder do
           Trestle.config.omniauth.providers.each do |name, args|
-            options = args.extract_options!
-            options[:path_prefix] = Trestle.config.path
-            self.provider name, *args
+            provider name, *args
           end
         end
       end
